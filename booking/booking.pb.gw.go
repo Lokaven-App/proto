ea56dc1f5ccfbae493ed2ceaa4cda367e3c537b3
Abriyanto Guntur <iannrafisyah.ir@gmail.com>
2020-07-17 18:08:26 +0800

    update proto booking

--- booking/booking.pb.gw.go
+++ booking/booking.pb.gw.go
@@ -637,6 +637,78 @@ func local_request_BookingRPC_GetAddonsList_0(ctx context.Context, marshaler run
 
 }
 
+var (
+	filter_BookingRPC_GetBookingReceipt_0 = &utilities.DoubleArray{Encoding: map[string]int{"order_number": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
+func request_BookingRPC_GetBookingReceipt_0(ctx context.Context, marshaler runtime.Marshaler, client BookingRPCClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ReceiptBooking
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["order_number"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "order_number")
+	}
+
+	protoReq.OrderNumber, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "order_number", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BookingRPC_GetBookingReceipt_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetBookingReceipt(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_BookingRPC_GetBookingReceipt_0(ctx context.Context, marshaler runtime.Marshaler, server BookingRPCServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ReceiptBooking
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["order_number"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "order_number")
+	}
+
+	protoReq.OrderNumber, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "order_number", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BookingRPC_GetBookingReceipt_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetBookingReceipt(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterBookingRPCHandlerServer registers the http handlers for service BookingRPC to "mux".
 // UnaryRPC     :call BookingRPCServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -780,6 +852,26 @@ func RegisterBookingRPCHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 		}
 
 		forward_BookingRPC_GetAddonsList_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_BookingRPC_GetBookingReceipt_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_BookingRPC_GetBookingReceipt_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_BookingRPC_GetBookingReceipt_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -964,6 +1056,26 @@ func RegisterBookingRPCHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("GET", pattern_BookingRPC_GetBookingReceipt_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_BookingRPC_GetBookingReceipt_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_BookingRPC_GetBookingReceipt_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -981,6 +1093,8 @@ var (
 	pattern_BookingRPC_GetPriceByTotalParticipant_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5}, []string{"api", "v1", "orders", "tourpackages", "tour_id", "prices-by-participant"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_BookingRPC_GetAddonsList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4, 1, 0, 4, 1, 5, 5, 2, 6}, []string{"api", "v1", "orders", "order_number", "tourpackages", "tour_id", "addons"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_BookingRPC_GetBookingReceipt_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"api", "v1", "orders", "order_number", "users"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -997,4 +1111,6 @@ var (
 	forward_BookingRPC_GetPriceByTotalParticipant_0 = runtime.ForwardResponseMessage
 
 	forward_BookingRPC_GetAddonsList_0 = runtime.ForwardResponseMessage
+
+	forward_BookingRPC_GetBookingReceipt_0 = runtime.ForwardResponseMessage
 )
--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module gitlab.com/lokaventour/proto
 go 1.12
 
 require (
-	github.com/golang/protobuf v1.3.4
+	github.com/golang/protobuf v1.4.1
 	github.com/grpc-ecosystem/grpc-gateway v1.13.0
-	google.golang.org/genproto v0.0.0-20200303153909-beee998c1893
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/grpc v1.27.1
+	google.golang.org/protobuf v1.25.0
 )
