45e9b0dcbd4422ddd9e3c7451b68c79b2d866eca
widimustopo <widi@aniqma.com>
2020-08-25 16:20:39 +0700

    add category pb

--- tour-package/tourpackage.pb.gw.go
+++ tour-package/tourpackage.pb.gw.go
@@ -662,6 +662,39 @@ func local_request_TourPackageService_UpdateTourPackage_0(ctx context.Context, m
 
 }
 
+var (
+	filter_TourPackageService_GetListCategory_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_TourPackageService_GetListCategory_0(ctx context.Context, marshaler runtime.Marshaler, client TourPackageServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq BrowseRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_TourPackageService_GetListCategory_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetListCategory(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_TourPackageService_GetListCategory_0(ctx context.Context, marshaler runtime.Marshaler, server TourPackageServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq BrowseRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_TourPackageService_GetListCategory_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetListCategory(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_WishListService_AddWishList_0(ctx context.Context, marshaler runtime.Marshaler, client WishListServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq AddWishListRequest
 	var metadata runtime.ServerMetadata
@@ -1008,6 +1041,26 @@ func RegisterTourPackageServiceHandlerServer(ctx context.Context, mux *runtime.S
 
 	})
 
+	mux.Handle("GET", pattern_TourPackageService_GetListCategory_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_TourPackageService_GetListCategory_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TourPackageService_GetListCategory_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1277,6 +1330,26 @@ func RegisterTourPackageServiceHandlerClient(ctx context.Context, mux *runtime.S
 
 	})
 
+	mux.Handle("GET", pattern_TourPackageService_GetListCategory_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_TourPackageService_GetListCategory_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TourPackageService_GetListCategory_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1298,6 +1371,8 @@ var (
 	pattern_TourPackageService_GetDetailHost_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4, 1, 0, 4, 1, 5, 5}, []string{"api", "v1", "tourpackages", "tour_id", "detail-tourhosts", "host_id"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_TourPackageService_UpdateTourPackage_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "tourpackages", "tour_id"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_TourPackageService_GetListCategory_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "tourpackages-categories"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -1318,6 +1393,8 @@ var (
 	forward_TourPackageService_GetDetailHost_0 = runtime.ForwardResponseMessage
 
 	forward_TourPackageService_UpdateTourPackage_0 = runtime.ForwardResponseMessage
+
+	forward_TourPackageService_GetListCategory_0 = runtime.ForwardResponseMessage
 )
 
 // RegisterWishListServiceHandlerFromEndpoint is same as RegisterWishListServiceHandler but
